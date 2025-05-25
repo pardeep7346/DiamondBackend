2b876ffe9c374beaabce17213e8c4c9eb05a62cc
pardeepsharma7346@gmail.com <user.email: pardeepsharma7346@gmail.com>
2025-05-25 21:18:54 +0530

    solving cors issue

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 import express from "express";
-import cors from 'cors'
-import cookieParser from "cookie-parser" 
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN, 
+app.use(
+  cors({
+    origin: "https://diamondinstitute.netlify.app", // or your exact frontend URL
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}))
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["set-cookie"],
+  })
+);
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
- 
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes
- 
-import userRouter from './Routes/user.routes.js'
-import adminRouter from './Routes/admin.routes.js'
-app.use("/users", userRouter); 
-app.use("/admin", adminRouter)
 
-export{app}
+import userRouter from "./Routes/user.routes.js";
+import adminRouter from "./Routes/admin.routes.js";
+app.use("/users", userRouter);
+app.use("/admin", adminRouter);
+
+export { app };
